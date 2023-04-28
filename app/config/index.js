6d69d2df16ec13206f2e66a01e5f0ebeb72363bb
HuyLam <tranlamhuy5tn@gmail.com>
2023-04-29 06:40:33 +0700

    update

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   app: {
-    port: process.env.PORT || 5050,
+    port: process.env.PORT || 'https://movies-app-.cyclic.app/',
   },
   db: {
     uri: process.env.MONGODB_URI || 'mongodb+srv://B2017044:EGXa8Mwc@cluster0.aiiqg4k.mongodb.net/Movies'
